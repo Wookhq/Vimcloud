ca8a173abc4f51a4e31a7ab1124d3c88f7f29b9c
evil chipo <itsjustchipoverhere@proton.me>
2025-11-29 09:05:33 +0700

    yay

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { browser } from '$app/environment';
+  import { PUBLIC_SUPABASE_PUBLISHABLE_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
+  import { createBrowserClient } from '@supabase/ssr';
+
+  export let data: { session: any };
+
+  let supabase: any = undefined;
+  if (browser) {
+    supabase = createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_PUBLISHABLE_KEY, {
+      global: { fetch }
+    });
+  }
+
+  const session = data.session;
+
+  async function signInDiscord() {
+    if (!supabase) return console.error('supabase not initialized');
+    await supabase.auth.signInWithOAuth({
+      provider: 'discord',
+      options: { redirectTo: location.origin }
+    });
+  }
+
+  async function signOut() {
+    if (!supabase) return;
+    await supabase.auth.signOut();
+    location.reload();
+  }
+</script>
+
+{#if session?.user}
+  <h2>Signed in</h2>
+  <p>Id: {session.user.id}</p>
+  <p>Email: {session.user.email}</p>
+  <button on:click={signOut}>Sign out</button>
+{:else}
+  <h1>Sign in</h1>
+  <button on:click={signInDiscord}>Sign in with Discord</button>
+{/if}
